67ed9d771624388d3fb3dac65627cdba44dd7500
Sofia-GMT <sofia.gonzalez.10147@gmail.com>
2023-12-05 18:53:23 +0100

    feat: remote test

--- defmap/tests/test_defmap.py
+++ defmap/tests/test_defmap.py
@@ -27,27 +27,38 @@ class TestDefmap(BaseTest):
 
         setupTestProject(cls)
 
-        cls.ds = DataSet.getDataSet('resmap')
+        cls.ds = DataSet.getDataSet('nma_V2.0')
         # Import Target EM map
         cls.protImportVol = cls.newProtocol(ProtImportVolumes, importFrom=ProtImportVolumes.IMPORT_FROM_FILES,
-                                       filesPath=cls.ds.getFile('betagal'),  samplingRate=2.0)
-        cls.protImportVol.setObjLabel('EM map')
+                                       filesPath=cls.ds.getFile('1ake_vol'),  samplingRate=2.0)
+        cls.protImportVol.setObjLabel('inputVolume')
         cls.launchProtocol(cls.protImportVol)
 
-        # cls.protImportVol = protImportVol
-        # cls.protImportPdb = cls.newProtocol(ProtImportPdb, inputPdbData=1,
-        #                                  pdbFile=cls.ds.getFile('pdb'))
-        # cls.protImportPdb.setObjLabel('Input PDB')
-        # cls.launchProtocol(cls.protImportPdb)
+        cls.protImportPdb = cls.newProtocol(ProtImportPdb, inputPdbData=1,
+                                         pdbFile=cls.ds.getFile('1ake_pdb'))
+        cls.protImportPdb.setObjLabel('inputStructure')
+        cls.launchProtocol(cls.protImportPdb)
 
 
     def testNothing(self):
         return
     
-    def testDefmapOk(self):
+    def testDefmap1(self):
+        # without atomic structure, threshold and resolution
         defmap = self.newProtocol(DefMapNeuralNetwork,
                                      inputVolume=self.protImportVol.outputVolume,
                                      # inputStructure=self.protImportPdb.outputPdb,
+                                     )
+        self.launchProtocol(defmap)
+        self.assertFalse(hasattr(defmap, "outputStructure"))
+
+    def testDefmap2(self):
+        # with atomic structure, threshold and resolution
+        defmap = self.newProtocol(DefMapNeuralNetwork,
+                                     inputVolume=self.protImportVol.outputVolume,
+                                     inputStructure=self.protImportPdb.outputPdb,
+                                     inputResolution=1,
+                                     inputThreshold=0.02
                                      )
         self.launchProtocol(defmap)
         self.assertTrue(hasattr(defmap, "outputStructure"))
